e472d6181a27fdac68e4df3facf0ec0e722485b4
TriSetianto123 <156288410+3nol04@users.noreply.github.com>
2025-06-02 06:40:52 +0700

    sadsd

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const serviceAccount = require('./servicesAccouns.json');
+const serviceAccount = require('./servicesAccountKey.json'); // Path to Firebase Admin SDK service account key
 
-// Inisialisasi Firebase Admin SDK
+// Initialize Firebase Admin SDK
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
@@ -14,20 +14,21 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+// Route to check if server is running
 app.get('/', (req, res) => {
     res.send('Hai, ini adalah REST API untuk Fasum!');
 });
 
+// Endpoint to send a notification
 app.post('/send-notification', async (req, res) => {
     const { token, title, body, roomId } = req.body;
 
-    // Validasi input
+    // Validate input
     if (!token || !title || !body) {
         return res.status(400).json({ error: 'Field token, title, dan body wajib diisi.' });
     }
 
-    // Payload untuk notifikasi
+    // Create message payload
     const message = {
         token,
         notification: {
@@ -51,7 +52,7 @@ app.post('/send-notification', async (req, res) => {
     };
 
     try {
-        // Mengirim pesan ke perangkat menggunakan Firebase Cloud Messaging (FCM)
+        // Send the message using Firebase Admin SDK
         const response = await admin.messaging().send(message);
         res.status(200).json({
             success: true,
@@ -66,7 +67,7 @@ app.post('/send-notification', async (req, res) => {
     }
 });
 
-// Menjalankan server pada port yang telah ditentukan
+// Start server
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
